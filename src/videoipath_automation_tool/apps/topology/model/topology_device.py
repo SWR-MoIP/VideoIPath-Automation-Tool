d05f5a1fe20bbf48507c7b3801c8b2fd6f86e963
Paul Winterstein <paul.winterstein@swr.de>
2025-02-12 12:13:20 +0100

    TopologyDevice and TopologyDeviceConfiguration refactoring (docstrings)

--- src/videoipath_automation_tool/apps/topology/model/topology_device.py
+++ src/videoipath_automation_tool/apps/topology/model/topology_device.py
@@ -8,7 +8,10 @@ from videoipath_automation_tool.apps.topology.model.topology_device_configuratio
 
 class TopologyDevice(BaseModel):
     """
-    Class which contains full information about a device in the topology
+    Class which contains full information about a device in the topology.
+
+    Attributes:
+        configuration (TopologyDeviceConfiguration): The configuration of the device
     """
 
     configuration: TopologyDeviceConfiguration
--- src/videoipath_automation_tool/apps/topology/model/topology_device_configuration.py
+++ src/videoipath_automation_tool/apps/topology/model/topology_device_configuration.py
@@ -2,6 +2,7 @@
 from typing import List, Literal
 
 from pydantic import BaseModel
+from typing_extensions import deprecated
 
 # Internal Imports
 from videoipath_automation_tool.apps.topology.model.n_graph_elements.topology_base_device import BaseDevice
@@ -14,21 +15,29 @@ from videoipath_automation_tool.apps.topology.model.n_graph_elements.topology_un
 
 
 class TopologyDeviceConfiguration(BaseModel):
-    """Class which contains the configuration of a device in the topology"""
+    """
+    Class which contains the configuration of a device in the topology
+
+    Attributes:
+        base_device (BaseDevice): Configuration of the base device (e.g. device label, description, appearance)
+        generic_vertices (List[GenericVertex]): Configuration of the generic vertices of the device
+        ip_vertices (List[IpVertex]): Configuration of the IP vertices of the device
+        codec_vertices (List[CodecVertex]): Configuration of the codec vertices of the device
+        internal_edges (List[UnidirectionalEdge]): Configuration of the internal unidirectional edges of the device (All edges that connect vertices within the same device)
+        external_edges (List[UnidirectionalEdge]): Configuration of the external unidirectional edges of the device (All edges that connect this device to other devices)
+    """
 
     base_device: BaseDevice
     generic_vertices: List[GenericVertex] = []
     ip_vertices: List[IpVertex] = []
     codec_vertices: List[CodecVertex] = []
-    internal_edges: List[
-        UnidirectionalEdge
-    ] = []  # List of edges that connect vertices within the same device (internal unidirectional edges)
-    external_edges: List[UnidirectionalEdge] = []  # List of edges that connect this device to other devices
+    internal_edges: List[UnidirectionalEdge] = []
+    external_edges: List[UnidirectionalEdge] = []
 
     # --- Setters / Getters ---
     @property
     def label(self):
-        """Get the manually set label of the device"""
+        """User defined label of the device"""
         return self.base_device.label
 
     @label.setter
@@ -37,7 +46,7 @@ class TopologyDeviceConfiguration(BaseModel):
 
     @property
     def description(self):
-        """Get the manually set description of the device"""
+        """User defined description of the device"""
         return self.base_device.description
 
     @description.setter
@@ -46,12 +55,12 @@ class TopologyDeviceConfiguration(BaseModel):
 
     @property
     def factory_label(self):
-        """Get the factory label of the device"""
+        """Factory label of the device"""
         return self.base_device.factory_label
 
     @property
     def factory_description(self):
-        """Get the factory description of the device"""
+        """Factory description of the device"""
         return self.base_device.factory_description
 
     # --- Methods ---
@@ -72,6 +81,44 @@ class TopologyDeviceConfiguration(BaseModel):
                 return element
         raise ValueError(f"Element with id '{id}' not found.")
 
+    def get_vertex_by_label(
+        self,
+        label: str,
+        mode: Literal["user_defined", "factory"] = "user_defined",
+        filter: Literal["all", "codec_vertex", "ip_vertex", "generic_vertex"] = "all",
+    ) -> CodecVertex | IpVertex | GenericVertex | None:
+        """Get a codec, ip or generic vertex by its label. The search can be filtered by vertex type and label type.
+        Method will return the first vertex found with the specified label.
+
+        Args:
+            label (str): The label of the vertex
+            mode (str, optional): Label type to search for (`user_defined` or `factory`). Defaults to `user_defined`.
+            filter (str, optional): Filter the search to a specific vertex type (`all`, `codec_vertex`, `ip_vertex`, `generic_vertex`). Defaults to `all`.
+
+        Returns:
+            CodecVertex | IpVertex | GenericVertex | None: The vertex with the specified label, or `None` if not found.
+        """
+
+        vertex_sets = [
+            (self.codec_vertices, "codec_vertex"),
+            (self.ip_vertices, "ip_vertex"),
+            (self.generic_vertices, "generic_vertex"),
+        ]
+
+        for vertices, filter_type in vertex_sets:
+            if filter in ("all", filter_type):
+                for vertex in vertices:
+                    if (mode == "user_defined" and vertex.label == label) or (
+                        mode == "factory" and vertex.factory_label == label
+                    ):
+                        return vertex
+
+        return None
+
+    @deprecated(
+        "The method `get_ip_vertex_by_label` is deprecated." " TODO,",
+        category=None,
+    )
     def get_ip_vertex_by_label(self, label: str) -> IpVertex:
         """Get an IpVertex by its label"""
         return_dict = {"in": None, "out": None}
