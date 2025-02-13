f01287c1edc552b5eade255399b84d14818352d0
Paul Winterstein <paul.winterstein@swr.de>
2025-02-13 22:24:10 +0100

    Refactor VideoIPathApp initialization

--- src/videoipath_automation_tool/apps/videoipath_app.py
+++ src/videoipath_automation_tool/apps/videoipath_app.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Optional
 
 from videoipath_automation_tool.apps.inventory.inventory_app import InventoryApp
 from videoipath_automation_tool.apps.preferences.preferences_app import PreferencesApp
@@ -9,21 +10,20 @@ from videoipath_automation_tool.settings import Settings
 
 
 class VideoIPathApp:
-    """Main class for VideoIPath Automation Tool.
-    VideoIPathApp contains all Apps and methods to interact with the VideoIPath System.
-    """
-
     def __init__(
         self,
-        server_address: str | None = None,
-        username: str | None = None,
-        password: str | None = None,
-        use_https=None,
-        verify_ssl_cert=None,
-        log_level: str | None = None,
-        environment=None,
+        server_address: Optional[str] = None,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        use_https: Optional[bool] = None,
+        verify_ssl_cert: Optional[bool] = None,
+        log_level: Optional[str] = None,
+        environment: Optional[str] = None,
     ):
-        """Initialize the VideoIPath Automation Tool, establish connection to the VideoIPath-Server and initialize the Apps for interaction.
+        """Main class for VideoIPath Automation Tool.
+        VideoIPathApp contains all Apps and methods to interact with the VideoIPath System.
+
+        Initialize the VideoIPath Automation Tool, establish connection to the VideoIPath-Server and initialize the Apps for interaction.
         Parameters can be provided directly or read from the environment variables.
 
         Args:
@@ -33,14 +33,17 @@ class VideoIPathApp:
             use_https (bool, optional): Set to `True` if the VideoIPath Server uses HTTPS. [ENV: VIPAT_USE_HTTPS]
             verify_ssl_cert (bool, optional): Set to `True` if the SSL certificate should be verified. [ENV: VIPAT_VERIFY_SSL_CERT]
             log_level (str, optional): The log level for the logging module, possible values are `DEBUG`, `INFO`, `WARNING`, `ERROR`, and `CRITICAL`. [ENV: VIPAT_LOG_LEVEL]
-            environment (str, optional): Define the environment: 'DEV', 'TEST', 'PROD'. [ENV: VIPAT_ENVIRONMENT]
+            environment (str, optional): Define the environment: `DEV`, `TEST`, `PROD`. [ENV: VIPAT_ENVIRONMENT]
         """
+
+        # --- Load environment variables ---
+        _settings = Settings()
 
         # --- Setup Logging ---
         log_level = (
             log_level.upper()
             if log_level
-            else (Settings().VIPAT_LOG_LEVEL.upper() if Settings().VIPAT_LOG_LEVEL else "INFO")
+            else (_settings.VIPAT_LOG_LEVEL.upper() if _settings.VIPAT_LOG_LEVEL else "INFO")
         )
 
         if log_level not in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]:
@@ -56,7 +59,7 @@ class VideoIPathApp:
         environment = (
             environment.upper()
             if environment
-            else (Settings().VIPAT_ENVIRONMENT.upper() if Settings().VIPAT_ENVIRONMENT else "DEV")
+            else (_settings.VIPAT_ENVIRONMENT.upper() if _settings.VIPAT_ENVIRONMENT else "DEV")
         )
 
         if environment not in ["DEV", "TEST", "PROD"]:
@@ -64,59 +67,66 @@ class VideoIPathApp:
 
         self._logger.debug(f"Environment set to '{environment}'.")
 
-        # --- Setup VideoIPath API Connector ---
+        # --- Initialize VideoIPath API Connector including check for connection and authentication ---
         self._logger.debug("Initialize VideoIPath API Connector.")
 
-        if server_address is None and Settings().VIPAT_VIDEOIPATH_SERVER_ADDRESS is None:
+        if server_address is None and _settings.VIPAT_VIDEOIPATH_SERVER_ADDRESS is None:
             raise ValueError(
                 "No address provided. Please provide an address or set it as an environment variable: 'VIPAT_VIDEOIPATH_SERVER_ADDRESS'."
             )
-        vip_server_address = (
-            server_address if server_address is not None else Settings().VIPAT_VIDEOIPATH_SERVER_ADDRESS
+        _vip_server_address = (
+            server_address if server_address is not None else _settings.VIPAT_VIDEOIPATH_SERVER_ADDRESS
         )
-        self._logger.debug(f"Server address: '{vip_server_address}'")
+        self._logger.debug(f"Server address: '{_vip_server_address}'")
 
-        if username is None and Settings().VIPAT_VIDEOIPATH_USERNAME is None:
+        if username is None and _settings.VIPAT_VIDEOIPATH_USERNAME is None:
             raise ValueError(
                 "No username provided. Please provide a username or set it as an environment variable: 'VIPAT_VIDEOIPATH_USERNAME'."
             )
-        vip_username = username if username is not None else Settings().VIPAT_VIDEOIPATH_USERNAME
-        self._logger.debug(f"Username: '{vip_username}'")
+        _vip_username = username if username is not None else _settings.VIPAT_VIDEOIPATH_USERNAME
+        self._logger.debug(f"Username: '{_vip_username}'")
 
-        if password is None and Settings().VIPAT_VIDEOIPATH_PASSWORD is None:
+        if password is None and _settings.VIPAT_VIDEOIPATH_PASSWORD is None:
             raise ValueError(
                 "No password provided. Please provide a password or set it as an environment variable: 'VIPAT_VIDEOIPATH_PASSWORD'."
             )
-        vip_password = password if password is not None else Settings().VIPAT_VIDEOIPATH_PASSWORD
+        _vip_password = password if password is not None else _settings.VIPAT_VIDEOIPATH_PASSWORD
         self._logger.debug("Password provided!")
 
-        use_https = use_https if use_https is not None else Settings().VIPAT_USE_HTTPS
+        use_https = use_https if use_https is not None else _settings.VIPAT_USE_HTTPS
         self._logger.debug("HTTPS enabled.") if use_https else self._logger.debug("HTTP enabled.")
 
-        verify_ssl_cert = verify_ssl_cert if verify_ssl_cert is not None else Settings().VIPAT_VERIFY_SSL_CERT
+        verify_ssl_cert = verify_ssl_cert if verify_ssl_cert is not None else _settings.VIPAT_VERIFY_SSL_CERT
         if use_https:
             self._logger.debug("Verify SSL certificate enabled.") if verify_ssl_cert else self._logger.debug(
                 "Verify SSL certificate disabled."
             )
 
-        # --- Initialize VideoIPath API Connector including check for connection and authentication ---
-        if vip_server_address is None:
+        if _vip_server_address is None:
             raise ValueError("No server address provided. Please provide a server address.")
 
-        if not vip_username:
+        if not _vip_username:
             raise ValueError("No username provided. Please provide a username.")
 
-        if not vip_password:
+        if not _vip_password:
             raise ValueError("No password provided. Please provide a password.")
 
         self._videoipath_connector = VideoIPathConnector(
-            server_address=vip_server_address,
-            username=vip_username,
-            password=vip_password,
+            server_address=_vip_server_address,
+            username=_vip_username,
+            password=_vip_password,
             use_https=use_https,
             verify_ssl_cert=verify_ssl_cert,
             logger=self._logger,
         )
+
+        # --- Reset the variables and settings ---
+        del server_address, _vip_server_address
+        del username, _vip_username
+        del password, _vip_password
+        del use_https
+        del verify_ssl_cert
+        del log_level
 
         # --- Initialize Apps ---
         self._inventory = InventoryApp(vip_connector=self._videoipath_connector, logger=self._logger)
@@ -124,9 +134,9 @@ class VideoIPathApp:
         self._preferences = PreferencesApp(vip_connector=self._videoipath_connector, logger=self._logger)
         self._profile = ProfileApp(vip_connector=self._videoipath_connector, logger=self._logger)
 
-        # --- Map API Methods to VideoIPathApp for easier access in Development Environment ---
-
+        # --- For Development, map the internal methods to the VideoIPathApp ---
         if environment == "DEV":
+            self.connector = self._videoipath_connector
             self.inventory_api = self._inventory._inventory_api
             self.topology_api = self._topology._topology_api
             self.preferences_api = self._preferences._preferences_api
