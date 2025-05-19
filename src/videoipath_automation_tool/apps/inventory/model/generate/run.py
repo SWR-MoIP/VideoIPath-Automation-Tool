ba9c3dd80610448fc5341c0ee1c56af47cdb7db8
Jonas Scholl <jonas@scholl.tech>
2025-05-19 17:05:56 +0200

    refactoring

--- src/videoipath_automation_tool/apps/inventory/model/generate/run.py
+++
@@ -1,7 +0,0 @@
-import json
-
-from videoipath_automation_tool.apps.inventory.model.generate.driver_model_generator import DriverModelGenerator
-
-if __name__ == "__main__":
-    schema = json.load(open("src/videoipath_automation_tool/apps/inventory/model/driver_schema/2024.3.3.json"))
-    DriverModelGenerator(schema=schema).generate()
