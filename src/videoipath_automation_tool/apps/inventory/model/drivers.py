05f744ec55a016f3ca75718af110a9a6e5dfbbf8
Paul Winterstein <paul.winterstein@swr.de>
2025-05-20 09:17:27 +0200

    fix: typo

--- src/videoipath_automation_tool/apps/inventory/model/drivers.py
+++ src/videoipath_automation_tool/apps/inventory/model/drivers.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel, Field
 
 # Notes:
 # - The name of the custom settings model follows the naming convention: CustomSettings_<driver_organization>_<driver_name>_<driver_version> => "." and "-" are replaced by "_"!
-# - /Users/jonas/dev/swr/VideoIPath-Automation-Tool/src/videoipath_automation_tool/apps/inventory/model/driver_schema/2024.1.4.json is used as reference to define the custom settings model!
+# - src/videoipath_automation_tool/apps/inventory/model/driver_schema/2024.1.4.json is used as reference to define the custom settings model!
 # - The "driver_id" attribute is necessary for the discriminator, which is used to determine the correct model for the custom settings in DeviceConfiguration!
 # - The "alias" attribute is used to map the attribute to the correct key (with driver organization & name) in the JSON payload for the API!
 # - "DriverLiteral" is used to provide a list of all possible drivers in the IDEs IntelliSense!
